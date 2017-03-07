9ca6a984be7287cff8c22522b27882785fd48a18
Curt Merrill <curt.merrill@turner.com>
2017-03-06 19:58:10 -0500

    specify start and end dates instead of number of days

--- fetch_data.py
+++ fetch_data.py
@@ -3,13 +3,17 @@
 import csv
 import os
 import requests
+
 from datetime import datetime, timedelta
+from pytz import timezone
 
 API_KEY = open(os.path.expanduser('~/.api-keys/forecast.io'), 'r').read().strip()
 COORDS = [35.908199, -75.668230]
+START_DATE = datetime(2016, 1, 1)
+END_DATE = datetime(2016, 12, 31)
 DAYS_HISTORY = 365
 
-API_URL = "https://api.darksky.net/forecast/{api_key}/{lat},{lon},{timestamp}"
+API_URL = "https://api.darksky.net/forecast/{api_key}/{lat},{lon},{timestamp}?exlude=currently,daily,flags"
 
 with open('wind_data.csv', 'w') as csv_file:
     fieldnames = [
@@ -25,25 +29,25 @@ with open('wind_data.csv', 'w') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
 
-    for days_ago in range(1, DAYS_HISTORY+1):
-        day = datetime.utcnow() - timedelta(days=days_ago)
+    day = START_DATE
+    while day <= END_DATE:
         response = requests.get(API_URL.format(
             api_key=API_KEY,
             lat=COORDS[0],
             lon=COORDS[1],
-            timestamp=day.replace(second=0, microsecond=0).isoformat()+'Z'
+            timestamp=day.strftime('%Y-%m-%dT00:01:00')
         ))
         if response.status_code != 200:
             print("ERROR fetching data for {}".format(day.strftime("%Y/%m/%d")))
         else:
+            response_tz = timezone(response.json()['timezone'])
+
             for hour in response.json()['hourly']['data']:
                 dt = datetime.fromtimestamp(hour['time'])
+                dt.replace(tzinfo=response_tz)
 
                 try:
-                    if int(hour['windSpeed']) == 0:
-                        wind_direction = ''
-                    else:
-                        wind_direction = hour['windBearing']
+                    wind_direction = hour['windBearing']
 
                     writer.writerow({
                         'posix_time': hour['time'],
@@ -56,4 +60,6 @@ with open('wind_data.csv', 'w') as csv_file:
                     })
                 except KeyError:
                     pass
+
+        day = day + timedelta(days=1)
 
